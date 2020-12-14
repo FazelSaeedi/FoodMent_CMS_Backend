7c328fe293ca26433af374edec8817321807c6f3
Fazel Saeedi <fazels36@gmail.com>
2020-12-14 20:23:17 +0330

    last commit for merge to master branch

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
 
-    Artisan::call('migrate');
+    //Artisan::call('migrate');
     //return view('welcome');
-    //abort(404);
+    abort(404);
 });
