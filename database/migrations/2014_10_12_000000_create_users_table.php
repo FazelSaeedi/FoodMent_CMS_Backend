10b5d7ccda8723306a9459f3b76b88926bdc371a
Fazel Saeedi <fazels36@gmail.com>
2020-12-16 20:11:48 +0330

    Create user_level table and update user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,12 +14,16 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
+
             $table->id();
+            $table->string('password', 60)->default('null');
             $table->string('phone')->unique()->nullable(false);
             $table->string('token' , '1000')->nullable();
             $table->string('sms_code')->unique()->nullable(false);
             $table->boolean('registerd');
+            $table->bigInteger('level_id')->default(1);
             $table->timestamps();
+
         });
     }
 
---
+++ database/migrations/2020_12_16_160353_create_users_level_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersLevelTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users_level', function (Blueprint $table) {
+            $table->id();
+            $table->integer('level');
+            $table->string('title');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users_level');
+    }
+}
