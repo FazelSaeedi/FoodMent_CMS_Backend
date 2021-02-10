93b2e8a05919d51c1bd2b0b61d1d6f437c06d834
FazelSaeedi <fazelSaeedi@iran.ir>
2021-02-11 00:34:12 +0330

    implement Restraunt model

--- app/Models/Restraunt.php
+++ app/Models/Restraunt.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Restraunt extends Model
 {
     use HasFactory;
+
+    public $timestamps = false;
+
+    protected $fillable = [
+        'name',
+        'admin',
+        'code',
+        'address',
+        'phone',
+        'update_at'
+    ];
+
 }
