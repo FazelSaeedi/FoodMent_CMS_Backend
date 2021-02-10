c5176ebc484cdca0827b87b35544612d43a5a23c
FazelSaeedi <fazelSaeedi@iran.ir>
2021-02-11 00:29:26 +0330

    update reinitialize function

---
+++ app/Http/Controllers/V1/ArtisanCommand.php
@@ -0,0 +1,61 @@
+<?php
+
+
+namespace App\Http\Controllers\V1;
+use App\Repository\UserRepository\UserRepositoryInterface;
+use Illuminate\Support\Facades\Artisan;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class ArtisanCommand extends Controller
+{
+    //
+    protected $userRepository ;
+
+
+    public function __construct(UserRepositoryInterface $userRepository)
+    {
+        $this->userRepository = $userRepository ;
+    }
+
+
+    public function initialize()
+    {
+    	$this->migrate();
+        $this->createUser();
+    }
+
+    public function reInitialize()
+    {
+        $this->remigrate();
+        $this->createUser();
+    }
+
+
+    public function migrate()
+    {
+    	return Artisan::call('migrate');
+    }
+
+
+    public function remigrate()
+    {
+        Artisan::call('migrate:fresh');
+    }
+
+
+    public function createUser()
+    {
+        $phoneNumber = '09112223333' ;
+        $password = '111' ;
+        $creatUser_Level = DB::statement("INSERT INTO `users_level` (`id`, `level`, `title`) VALUES ('1', '10', 'admin')");
+        $createUser =  $this->userRepository->createUser($phoneNumber);
+        $smsCode = rand ( 1001 , 9999 );
+        $setSmsCode = $this->userRepository->setSmsCode($smsCode , $phoneNumber);
+        $hashPassword = Hash::make($password);
+        $setUserPassword = $this->userRepository->setUserPassword($phoneNumber , $hashPassword);
+        return 'done';
+    }
+}
