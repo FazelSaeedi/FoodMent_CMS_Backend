b8fd619ac5e9bdd373b6b00f950970fe93701280
Fazel Saeedi <fazelsaeedi@iran.ir>
2021-02-25 16:28:14 -0500

    add global base_url into menu .blade

--- resources/views/V1/partials/menu.blade.php
+++ resources/views/V1/partials/menu.blade.php
@@ -1,12 +1,14 @@
 
-
+<?php
+$url = Config::get('app.base_url');
+?>
 <nav class="sidenav navbar navbar-vertical  fixed-left  navbar-expand-xs navbar-light bg-white" id="sidenav-main">
     <div class="scrollbar-inner">
 
         <!-- Brand -->
         <div class="sidenav-header  align-items-center">
             <a class = "navbar-brand" href="javascript:void(0)">
-                <img src="/V1/assets/img/brand/foodment.png" class="navbar-brand-img" alt="...">
+                <img src="{{$url}}/V1/assets/img/brand/foodment.png" class="navbar-brand-img" alt="...">
             </a>
         </div>
         <style>
@@ -28,37 +30,37 @@
                 <!-- Nav items -->
                 <ul class="navbar-nav">
                     <li id="menu-panel" class="nav-item">
-                        <a  class="nav-link" href="/v1/profile/home">
+                        <a  class="nav-link" href="{{$url}}/v1/profile/home">
 {{--                            <i class="ni ni-tv-2 text-primary"></i>--}}
                             <span class="nav-link-text">پنل کاربری</span>
                         </a>
                     </li>
                     <li id="menu-type" class="nav-item">
-                        <a class="nav-link" href="/v1/profile/types">
+                        <a class="nav-link" href="{{$url}}/v1/profile/types">
 {{--                            <i class="ni ni-planet text-orange"></i>--}}
                             <span class="nav-link-text"> دسته بندی ها</span>
                         </a>
                     </li>
                     <li id="menu-maingroup" class="nav-item">
-                        <a class="nav-link" href="/v1/profile/maingroups">
+                        <a class="nav-link" href="{{$url}}/v1/profile/maingroups">
 {{--                            <i class="ni ni-planet text-orange"></i>--}}
                             <span class="nav-link-text">گروه ها</span>
                         </a>
                     </li>
                     <li id="menu-subgroup" class="nav-item">
-                        <a class="nav-link" href="/v1/profile/subgroups">
+                        <a class="nav-link" href="{{$url}}/v1/profile/subgroups">
 {{--                            <i class="ni ni-planet text-orange"></i>--}}
                             <span class="nav-link-text"> زیر گروه ها</span>
                         </a>
                     </li>
                     <li id="menu-product" class="nav-item">
-                        <a class="nav-link" href="/v1/profile/products">
+                        <a class="nav-link" href="{{$url}}/v1/profile/products">
 {{--                            <i class="ni ni-planet text-orange"></i>--}}
                             <span class="nav-link-text"> محصولات ها</span>
                         </a>
                     </li>
                     <li  id="menu-restraunt" class="nav-item">
-                        <a class="nav-link" href="/v1/profile/restraunts">
+                        <a class="nav-link" href="{{$url}}/v1/profile/restraunts">
 {{--                            <i class="ni ni-spaceship text-orange"></i>--}}
                             <span class="nav-link-text">رستوران ها</span>
                         </a>
