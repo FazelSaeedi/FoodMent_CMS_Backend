084a6d2a6fcff6634dfa551afee078229d9c68ed
Fazel Saeedi <FazelSaeedi@iran.ir>
2021-04-26 21:03:14 +0430

    implement restaurantAcceptOrder function in orderController

--- app/Http/Controllers/V1/OrderController.php
+++ app/Http/Controllers/V1/OrderController.php
@@ -5,11 +5,14 @@ namespace App\Http\Controllers\V1;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\V1\GetAllRestrauntOrdersRequest;
 use App\Http\Requests\V1\GetNewRestrauntOrdersRequest;
+use App\Http\Requests\V1\restaurantAcceptOrderRequest;
 use App\Models\Order;
 use App\Repository\OrderRepository\OrderRepositoryInterface;
 use App\ToViewGenerator\MessageController;
+use App\ToViewGenerator\Views\AcceptRestrauntOrderViewModel;
 use App\ToViewGenerator\Views\allRestrauntOrdersViewModel;
 use App\ToViewGenerator\Views\newRestrauntOrdersViewModel;
+use Illuminate\Http\Response;
 
 
 class OrderController extends Controller
@@ -90,6 +93,24 @@ class OrderController extends Controller
         return MessageController::sendMessage(200 , [] , [
             $getAllOrders , $getAllOrderItems
         ] , allRestrauntOrdersViewModel::class );
+
+    }
+
+
+
+    public function restaurantAcceptOrder( restaurantAcceptOrderRequest $request , $restaurantCode , $orderId , $status)
+    {
+        $result = false ;
+
+        if ($status == true)
+            $result  = $this->orderRepository->restaurantAcceptOrder($orderId);
+        else
+            $result = $this->orderRepository->restaurantCanselOrder($orderId);
+
+        if ($result)
+            return MessageController::sendMessage(200 , [] , [] , AcceptRestrauntOrderViewModel::class );
+        else
+            return MessageController::sendMessage(Response::HTTP_INTERNAL_SERVER_ERROR , [] , [] , AcceptRestrauntOrderViewModel::class );
 
     }
 
