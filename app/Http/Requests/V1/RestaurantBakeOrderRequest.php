1ac89676c4b832495a391c7fe94a29a51d4d86cb
Fazel Saeedi <FazelSaeedi@iran.ir>
2021-04-30 16:29:58 +0430

    Change name of these 2 class

--- app/Http/Requests/V1/RestaurantBakeOrderRequest.php
+++ app/Http/Requests/V1/RestaurantBakeOrderRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests\V1;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class RestaurantBakeOrder extends FormRequest
+class RestaurantBakeOrderRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
--- app/Http/Requests/V1/RestaurantSendOrderRequest.php
+++ app/Http/Requests/V1/RestaurantSendOrderRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests\V1;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class RestaurantSendOrder extends FormRequest
+class RestaurantSendOrderRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
