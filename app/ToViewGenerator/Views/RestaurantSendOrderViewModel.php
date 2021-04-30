dc41779663a85ebd738d5baa8897f15ceba297ca
Fazel Saeedi <FazelSaeedi@iran.ir>
2021-04-30 16:29:15 +0430

    Create && implement RestaurantBakeOrderViewModel && RestaurantSendOrderViewModel

---
+++ app/ToViewGenerator/Views/RestaurantSendOrderViewModel.php
@@ -0,0 +1,40 @@
+<?php
+
+
+namespace App\ToViewGenerator\Views;
+
+
+use App\ToViewGenerator\BaseMessage;
+
+class RestaurantSendOrderViewModel implements BaseMessage
+{
+
+
+    private array $data;
+
+
+    public function __construct($data)
+    {
+        $this->data = $data;
+    }
+
+
+    public function getJsonMobileView()
+    {
+        return [];
+    }
+
+
+    public function getJsonDeviceView()
+    {
+        return [];
+    }
+
+
+    public function getJsonWebBrowserView()
+    {
+        return [];
+    }
+
+
+}
