ec6ebe6891aab644bf56d66b644afdc043295415
Fazel Saeedi <FazelSaeedi@iran.ir>
2021-05-04 20:13:58 +0430

    Create cities Table Migration

---
+++ database/migrations/2021_05_04_074025_create_cities_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cities', function (Blueprint $table) {
+
+            $table->integer("id" , true)->unique();
+            $table->string('name','255');
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cities');
+    }
+}
