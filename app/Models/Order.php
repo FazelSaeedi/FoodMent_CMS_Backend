b22a493068672b1c439854ec82cd22bcc6d9747f
Fazel Saeedi <FazelSaeedi@iran.ir>
2021-05-08 04:29:01 +0430

    fill fillable property of Order Model

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -9,6 +9,16 @@ class Order extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'userid' ,
+        'totalamount' ,
+        'totalprice' ,
+        'restraunt_id' ,
+        'restraunt_code',
+        'address_id' ,
+        'userdescription'
+    ];
+
     public function OrderItems()
     {
         return $this->hasMany(OrderItem::class);
